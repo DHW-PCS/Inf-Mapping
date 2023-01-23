2b0458b9180539aa64033c4ac4ad45299d4ba735
sjfhsjfh <sjfhsjfh@qq.com>
2023-01-23 14:11:26 +0800

    (fix) Make it available as a module?

--- scripts/python/lib.py
+++ scripts/python/lib.py
@@ -2,7 +2,7 @@ from xmlrpc.client import Boolean
 
 
 class Waypoint:
-    def __init__(self, pos: tuple, dim: str, name: str, desc: str = "", *args, **kwargs) -> "Waypoint":
+    def __init__(self, pos: tuple = (0, 0, 0), dim: str = "overworld", name: str = "LOL", desc: str = "", *args, **kwargs) -> "Waypoint":
         """
         DO NOT USE args PLZZZZZZZ
         kwargs is used to save different map options
@@ -19,6 +19,10 @@ class Waypoint:
                 pass
         self.opts = kwargs
 
+    def __str__(self) -> str:
+        # return f"""{self.name}"""
+        return str(self.__dict__)
+
     def is_same_point(self, waypoint) -> Boolean:
         return self.pos == waypoint.pos
 
@@ -30,12 +34,20 @@ class Waypoint:
 
 def join_list(wplist1: list, wplist2: list) -> list:
     """
-    Join 2 waypoint list
+    Join 2 waypoint list, wplist1 would be ignored if there is a conflict
     """
     wpdict = {}
     wplist1.extend(wplist2)
     for waypoint in wplist1:
-        waypoint = Waypoint(**waypoint)
+        if type(waypoint) == dict:
+            # Restore the object
+            _temp = Waypoint()
+            for prop in waypoint:
+                _temp.__dict__[prop] = waypoint[prop]
+            waypoint = _temp
+            del _temp
+        else:
+            waypoint = Waypoint(**waypoint)
         if waypoint.loc not in wpdict:
             wpdict[waypoint.loc] = waypoint
         elif waypoint.name == wpdict[waypoint.loc].name and waypoint.desc == wpdict[waypoint.loc].desc:
@@ -43,6 +55,7 @@ def join_list(wplist1: list, wplist2: list) -> list:
             opts = wpdict[waypoint.loc].opts
             for opt in waypoint.opts:
                 opts[opt] = waypoint.opts[opt]
+            wpdict[waypoint.loc].opts = opts
         else:
             print(
                 f"Conflict at joining waypoints <{waypoint.name}> and <{wpdict[waypoint.loc].name}>")
--- scripts/python/xaeros.py
+++ scripts/python/xaeros.py
@@ -10,8 +10,22 @@ dims = {
     "nether": "dim%-1",
     "end": "dim%1"
 }
+xaeros_opt_default = {
+    "color": "1",
+    "disabled": "false",
+    "type": "0",
+    "set": "gui.xaero_default",
+    "rotate_on_tp": "false",
+    "tp_yaw": "0",
+    "global": "false",
+    "visibility_type": 0
+}
 HEADER = """#
 #waypoint:name:initials:x:y:z:color:disabled:type:set:rotate_on_tp:tp_yaw:global
+#
+"""
+HEADER_2 = """#
+#waypoint:name:initials:x:y:z:color:disabled:type:set:rotate_on_tp:tp_yaw:visibility_type
 #
 """
 dir = os.path.dirname(__file__)
@@ -25,8 +39,11 @@ def load_str(raw_str: str, dim: str) -> Waypoint:
     str_list = raw_str.split(":")
     str_list = list(map(lambda x: x.replace("§§", ":"), str_list))
     str_list.pop(0)  # "waypoint"
-    name = str_list.pop(0)
-    abbr = str_list.pop(0)
+    try:
+        name = str_list.pop(0)
+        abbr = str_list.pop(0)
+    except:
+        print(raw_str)
     try:
         x = round(float(str_list.pop(0)))
     except:
@@ -54,15 +71,7 @@ def dump_str(waypoint: Waypoint) -> str:
     """
     Dump a Waypoint Object into Xaeros string
     """
-    xaeros_opt = {
-        "color": "1",
-        "disabled": "false",
-        "type": "0",
-        "set": "gui.xaero_default",
-        "rotate_on_tp": "false",
-        "tp_yaw": "0",
-        "global": "false"
-    }
+    xaeros_opt = xaeros_opt_default
     if "xaeros_opt" in waypoint.opts:
         for opt in waypoint.opts["xaeros_opt"]:
             xaeros_opt[opt] = waypoint.opts["xaeros_opt"][opt]
@@ -79,14 +88,15 @@ def read_all() -> dict:
     for dim in dims:
         if os.path.isdir(os.path.join(dir, dims[dim])):
             with open(os.path.join(dir, dims[dim], "mw$default_1.txt"), "r") as f:
-                raw_list = f.read().split(HEADER)[-1].strip().split("\n")
+                raw_list = f.read().split(
+                    HEADER)[-1].split(HEADER_2)[-1].strip().split("\n")
             wplist.extend(map(lambda x: load_str(x, dim).__dict__, raw_list))
     data["waypoints"] = list(
         map(lambda x: x.__dict__, join_list(data["waypoints"], wplist)))
     return data
 
 
-def write_all(data: dict):
+def output(data: dict):
     """
     Write the `data` as the Xaeros format in the `__file__` directory
     """
@@ -107,3 +117,9 @@ def write_all(data: dict):
             with open(os.path.join(build_dir, dims[dim], "mw$default_1.txt"), "w") as f:
                 f.write(
                     HEADER + "\n".join(map(lambda x: dump_str(Waypoint(**x)), wpdict[dim])) + "\n")
+
+
+def update() -> None:
+    data = read_all()
+    with open(os.path.join(dir, "..", "..", "data", "data.json"), 'w') as f:
+        f.write(json.dumps(data, ensure_ascii=False))
